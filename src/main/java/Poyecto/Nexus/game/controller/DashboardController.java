bd59efafe559e468c626d6abc8d77c3f33689d27
Brandon_L205 <brandon.lema1647@utc.edu.ec>
2026-07-15 10:22:31 -0500

    Creacion del HTML catalogo y modificacion de direccionamiento en el dasboard

--- src/main/java/Poyecto/Nexus/game/controller/DashboardController.java
+++ src/main/java/Poyecto/Nexus/game/controller/DashboardController.java
@@ -6,21 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
-    // 1. Mapea la raíz para que al entrar a http://localhost:8080/ cargue tu index.html
     @GetMapping("/")
     public String showIndex() {
-        return "index"; // Busca el archivo index.html en la carpeta templates
+        return "index"; 
     }
 
-    // 2. Mapea también http://localhost:8080/index por si acaso
     @GetMapping("/index")
     public String showIndexAlternativo() {
         return "index";
     }
 
-    // 3. Tu página principal de administración en http://localhost:8080/admin
+    // NUEVA RUTA PARA EL CATÁLOGO COMPLETO
+    @GetMapping("/catalogo")
+    public String showCatalogo() {
+        return "catalogo"; // Retornará templates/catalogo.html
+    }
+
     @GetMapping("/admin")
     public String showDashboard() {
-        return "dashboard"; // Busca el archivo dashboard.html en templates
+        return "dashboard"; 
     }
 }
